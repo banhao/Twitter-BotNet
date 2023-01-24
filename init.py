94c146e5d62193e12a72a06baf7f71f75b949554
banhao <banhao@gmail.com>
2023-01-23 22:31:42 -0600

    Add files via upload

---
+++ init.py
@@ -0,0 +1,9 @@
+#Twitter Bot Configuration
+Twitter_API_key = ""
+Twitter_API_key_secret = ""
+Twitter_Bearer_Token = ""
+Twitter_auth_key = ""
+Twitter_auth_secrett = ""
+Twitter_Client_ID = ""
+Twitter_Client_Secret = ""
+Bot_List = {'username01':'password', 'username02':'password', 'username03':'password', 'username04':'password', 'username05':'password'}
